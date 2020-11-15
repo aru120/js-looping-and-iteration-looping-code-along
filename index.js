567a37d9009d12b3d83301f1da12382d528e0af5
aru120 <khuang159@gmail.com>
2020-11-15 14:28:23 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+names = ["John","babe"]
+
+function writeCards(arr, event){
+    let newarr =[]
+    for (let i = 0; i < arr.length; i++){
+        newarr.push(`Thank you, ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return newarr
+}
+
+
+function countDown(){
+    let num = 0
+    while(num < 11){
+        console.log(num)
+        num++
+    }
+}
